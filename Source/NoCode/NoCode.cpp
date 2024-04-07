bfaa1cf67cf6314e08b3338870794b29e98cfd54
Chojun <chojun917@gmail.com>
2024-04-07 22:33:49 +0900

    MappingContext

---
+++ Source/NoCode/NoCode.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "NoCode.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NoCode, "NoCode" );
---
+++ Source/NoCode/Private/Pawns/Bird.cpp
@@ -0,0 +1,63 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Pawns/Bird.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+
+// Sets default values
+ABird::ABird()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Capsule = CreateDefaultSubobject<UCapsuleComponent>(TEXT("CapsuleComponent"));
+	Capsule->SetCapsuleHalfHeight(20);
+	Capsule->SetCapsuleRadius(15); //높이, 크기	
+	SetRootComponent(Capsule);
+
+	BirdMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("BirdMesh"));
+	BirdMesh->SetupAttachment(Capsule); //초기화
+	//BirdMesh->AttachToComponent(Capsule); //동적으로 컴포넌트 추가할때
+
+	//AutoPossessPlayer = EAutoReceiveInput::Player0; //player 0 자동빙의
+}
+
+void ABird::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	APlayerController* PlayerController = Cast<APlayerController>(GetController()); //현재 캐릭터의 컨트롤러
+	if(PlayerController)
+	{
+		UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()); //향상된 로컬 플레이어 서브시스템 가져오기
+		if(Subsystem)
+		{
+			Subsystem->AddMappingContext(BirdMappingContext, 0); //매핑컨텍스트 추가
+		}
+	}
+}
+
+// Called every frame
+void ABird::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ABird::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+void ABird::Move(const FInputActionValue& Value)
+{
+	const bool CurrentValue = Value.Get<bool>();
+	if(CurrentValue)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("IA_Move"));
+	}
+}
